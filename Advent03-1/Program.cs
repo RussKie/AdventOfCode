3a2309f02e07de8f90c1cace6ee9b84e87e644cd
Igor Velikorossov <igor.velikorossov@microsoft.com>
2024-12-03 22:09:49 -0800

    Advent03-1

---
+++ Advent03-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string[] lines = File.ReadAllLines("input.txt");
+
+        long total = 0;
+        foreach (string line in lines)
+        {
+            var matches = Regex.Matches(line, "mul\\((?<v1>\\d{1,3}),(?<v2>\\d{1,3})\\)");
+            Console.WriteLine(line);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"  --> {match.Value}");
+
+                int mul = int.Parse(match.Groups["v1"].Value) * int.Parse(match.Groups["v2"].Value);
+                Console.WriteLine($"  --> {mul}");
+                total += mul;
+            }
+        }
+
+        Console.WriteLine($"Total: {total}");
+    }
+}
